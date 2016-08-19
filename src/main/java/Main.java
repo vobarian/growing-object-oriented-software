a5599712628148255bcde3167e0a75d8829325c1
Chad Berchek <chadberchek@carfax.com>
2016-08-19 13:05:27 -0500

    p. 99 make connection

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,21 @@
+import org.jivesoftware.smack.Chat;
+import org.jivesoftware.smack.MessageListener;
+import org.jivesoftware.smack.XMPPConnection;
+import org.jivesoftware.smack.XMPPException;
+import org.jivesoftware.smack.packet.Message;
+
 import javax.swing.SwingUtilities;
 
 public class Main {
+    private static final int ARG_HOSTNAME = 0;
+    private static final int ARG_USERNAME = 1;
+    private static final int ARG_PASSWORD = 2;
+    private static final int ARG_ITEM_ID = 3;
+
+    public static final String AUCTION_RESOURCE = "Auction";
+    public static final String ITEM_ID_AS_LOGIN = "auction-%s";
+    public static final String AUCTION_ID_FORMAT = ITEM_ID_AS_LOGIN + "@%s/" + AUCTION_RESOURCE;
+
     private MainWindow ui;
 
     public Main() throws Exception {
@@ -9,6 +24,27 @@ public class Main {
 
     public static void main(String... args) throws Exception {
         Main main = new Main();
+        XMPPConnection connection = connectTo(args[ARG_HOSTNAME], args[ARG_USERNAME], args[ARG_PASSWORD]);
+        Chat chat = connection.getChatManager().createChat(
+                auctionId(args[ARG_ITEM_ID], connection),
+                new MessageListener() {
+                    @Override
+                    public void processMessage(Chat aChat, Message message) {
+
+                    }
+                });
+        chat.sendMessage(new Message());
+    }
+
+    private static XMPPConnection connectTo(String hostname, String username, String password) throws XMPPException {
+        XMPPConnection connection = new XMPPConnection(hostname);
+        connection.connect();
+        connection.login(username, password, AUCTION_RESOURCE);
+        return connection;
+    }
+
+    private static String auctionId(String itemId, XMPPConnection connection) {
+        return String.format(AUCTION_ID_FORMAT, itemId, connection.getServiceName());
     }
 
     private void startUserInterface() throws Exception {
