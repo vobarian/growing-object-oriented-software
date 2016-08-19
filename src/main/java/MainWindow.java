741d717e19870057cf8990c78febd148088944d4
Chad Berchek <chadberchek@carfax.com>
2016-08-19 16:39:52 -0500

    p. 109 add test for join, bid, lose

--- src/main/java/MainWindow.java
+++ src/main/java/MainWindow.java
@@ -8,6 +8,7 @@ public class MainWindow extends JFrame {
     public static final String STATUS_JOINING = "Joining";
     public static final String STATUS_LOST = "Lost";
     public static final String SNIPER_STATUS_NAME = "sniper status";
+    public static final String STATUS_BIDDING = "Bidding";
 
     private final JLabel sniperStatus = createLabel(STATUS_JOINING);
 
