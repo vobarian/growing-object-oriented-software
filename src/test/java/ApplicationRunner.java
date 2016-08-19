6c2a2fdf95e7550184abc749d0f04d8ff0b503e6
Chad Berchek <chadberchek@carfax.com>
2016-08-19 11:37:06 -0500

    First test

---
+++ src/test/java/ApplicationRunner.java
@@ -0,0 +1,33 @@
+public class ApplicationRunner {
+    public static final String XMPP_HOSTNAME = "localhost";
+    public static final String SNIPER_ID = "sniper";
+    public static final String SNIPER_PASSWORD = "sniper";
+    private AuctionSniperDriver driver;
+
+    public void startBiddingIn(final FakeAuctionServer auction) {
+        Thread thread = new Thread("Test Application") {
+            @Override
+            public void run() {
+                try {
+                    Main.main(XMPP_HOSTNAME, SNIPER_ID, SNIPER_PASSWORD, auction.getItemId());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        thread.setDaemon(true);
+        thread.start();
+        driver = new AuctionSniperDriver(1000);
+        driver.showsSniperStatus(MainWindow.STATUS_JOINING);
+    }
+
+    public void showsSniperHasLostAuction() {
+        driver.showsSniperStatus(MainWindow.STATUS_LOST);
+    }
+
+    public void stop() {
+        if (driver != null) {
+            driver.dispose();
+        }
+    }
+}
---
+++ src/test/java/AuctionSniperDriver.java
@@ -0,0 +1,20 @@
+import com.objogate.wl.swing.AWTEventQueueProber;
+import com.objogate.wl.swing.driver.JFrameDriver;
+import com.objogate.wl.swing.driver.JLabelDriver;
+import com.objogate.wl.swing.gesture.GesturePerformer;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+
+public class AuctionSniperDriver extends JFrameDriver {
+    public AuctionSniperDriver(int timeoutMillis) {
+        super(new GesturePerformer(),
+                JFrameDriver.topLevelFrame(
+                        named(MainWindow.MAIN_WINDOW_NAME),
+                        showingOnScreen()),
+                new AWTEventQueueProber(timeoutMillis, 100));
+    }
+
+    public void showsSniperStatus(String statusText) {
+        new JLabelDriver(this, named(MainWindow.SNIPER_STATUS_NAME)).hasText(equalTo(statusText));
+    }
+}
