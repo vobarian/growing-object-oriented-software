4c5f148c3ecefad0fd1f80a11934d08eb3433334
Chad Berchek <chadberchek@carfax.com>
2016-08-19 13:15:41 -0500

    p. 102 receive message and update label

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -17,6 +17,7 @@ public class Main {
     public static final String AUCTION_ID_FORMAT = ITEM_ID_AS_LOGIN + "@%s/" + AUCTION_RESOURCE;
 
     private MainWindow ui;
+    @SuppressWarnings("unused") private Chat notToBeGCd;
 
     public Main() throws Exception {
         startUserInterface();
@@ -24,19 +25,29 @@ public class Main {
 
     public static void main(String... args) throws Exception {
         Main main = new Main();
-        XMPPConnection connection = connectTo(args[ARG_HOSTNAME], args[ARG_USERNAME], args[ARG_PASSWORD]);
-        Chat chat = connection.getChatManager().createChat(
-                auctionId(args[ARG_ITEM_ID], connection),
+        main.joinAuction(connection(args[ARG_HOSTNAME], args[ARG_USERNAME], args[ARG_PASSWORD]), args[ARG_ITEM_ID]);
+    }
+
+    private void joinAuction(XMPPConnection connection, String itemId) throws XMPPException {
+        final Chat chat = connection.getChatManager().createChat(
+                auctionId(itemId, connection),
                 new MessageListener() {
                     @Override
                     public void processMessage(Chat aChat, Message message) {
-
+                        SwingUtilities.invokeLater(new Runnable() {
+                            @Override
+                            public void run() {
+                                ui.showStatus(MainWindow.STATUS_LOST);
+                            }
+                        });
                     }
                 });
+        this.notToBeGCd = chat;
+
         chat.sendMessage(new Message());
     }
 
-    private static XMPPConnection connectTo(String hostname, String username, String password) throws XMPPException {
+    private static XMPPConnection connection(String hostname, String username, String password) throws XMPPException {
         XMPPConnection connection = new XMPPConnection(hostname);
         connection.connect();
         connection.login(username, password, AUCTION_RESOURCE);
