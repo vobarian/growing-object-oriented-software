553faf5a86146ed333cdd65fc2c99aeb5fc6fd18
Chad Berchek <chadberchek@carfax.com>
2016-08-19 11:43:36 -0500

    p. 97 create window

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,22 @@
-public class Main {
-    public static void main(String... args) {
+import javax.swing.SwingUtilities;
 
+public class Main {
+    private MainWindow ui;
+
+    public Main() throws Exception {
+        startUserInterface();
+    }
+
+    public static void main(String... args) throws Exception {
+        Main main = new Main();
+    }
+
+    private void startUserInterface() throws Exception {
+        SwingUtilities.invokeAndWait(new Runnable() {
+            @Override
+            public void run() {
+                ui = new MainWindow();
+            }
+        });
     }
 }
--- src/main/java/MainWindow.java
+++ src/main/java/MainWindow.java
@@ -1,7 +1,15 @@
-public class MainWindow {
+import javax.swing.JFrame;
+
+public class MainWindow extends JFrame {
     public static final String MAIN_WINDOW_NAME = "Auction Sniper Main";
     public static final String STATUS_JOINING = "Joining";
     public static final String STATUS_LOST = "Lost";
     public static final String SNIPER_STATUS_NAME = "sniper status";
 
+    public MainWindow() {
+        super("Auction Sniper");
+        setName(MAIN_WINDOW_NAME);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
 }
