4b43e3578ee7bda697c0bc1a231b003e4876917a
Chad Berchek <chadberchek@carfax.com>
2016-08-19 11:37:06 -0500

    First test

---
+++ src/main/java/MainWindow.java
@@ -0,0 +1,7 @@
+public class MainWindow {
+    public static final String MAIN_WINDOW_NAME = "Auction Sniper Main";
+    public static final String STATUS_JOINING = "Joining";
+    public static final String STATUS_LOST = "Lost";
+    public static final String SNIPER_STATUS_NAME = "sniper status";
+
+}
---
+++ src/test/java/FakeAuctionServer.java
@@ -0,0 +1,73 @@
+import org.jivesoftware.smack.Chat;
+import org.jivesoftware.smack.ChatManagerListener;
+import org.jivesoftware.smack.MessageListener;
+import org.jivesoftware.smack.XMPPConnection;
+import org.jivesoftware.smack.XMPPException;
+import org.jivesoftware.smack.packet.Message;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.TimeUnit;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class FakeAuctionServer {
+    public static final String ITEM_ID_AS_LOGIN = "auction-%s";
+    public static final String AUCTION_RESOURCE = "Auction";
+    public static final String XMPP_HOSTNAME = "localhost";
+    private static final String AUCTION_PASSWORD = "auction";
+
+    private final SingleMessageListener messageListener = new SingleMessageListener();
+    private final String itemId;
+    private final XMPPConnection connection;
+    private Chat currentChat;
+
+    public FakeAuctionServer(String itemId) {
+        this.itemId = itemId;
+        this.connection = new XMPPConnection(XMPP_HOSTNAME);
+    }
+
+    public void startSellingItem() throws XMPPException {
+        connection.connect();
+        connection.login(String.format(ITEM_ID_AS_LOGIN, itemId),
+                AUCTION_PASSWORD, AUCTION_RESOURCE);
+        connection.getChatManager().addChatListener(
+                new ChatManagerListener() {
+                    @Override
+                    public void chatCreated(Chat chat, boolean createdLocally) {
+                        currentChat = chat;
+                        chat.addMessageListener(messageListener);
+                    }
+                });
+    }
+
+    public String getItemId() {
+        return itemId;
+    }
+
+    public void hasReceivedJoinRequestFromSniper() throws InterruptedException {
+        messageListener.receivesAMessage();
+    }
+
+    public void announceClosed() throws XMPPException {
+        currentChat.sendMessage(new Message());
+    }
+
+    public void stop() {
+        connection.disconnect();
+    }
+
+    public class SingleMessageListener implements MessageListener {
+        private final ArrayBlockingQueue<Message> messages =
+                new ArrayBlockingQueue<Message>(1);
+
+        public void processMessage(Chat chat, Message message) {
+            messages.add(message);
+        }
+
+        public void receivesAMessage() throws InterruptedException {
+            assertThat("Message", messages.poll(5, TimeUnit.SECONDS), is(notNullValue()));
+        }
+    }
+}
