05d37c6479d6b693c9408ba46992a9f9c2d41654
Chad Berchek <chadberchek@carfax.com>
2016-08-19 19:35:56 -0500

    p. 115 first unit test for AuctionMessageTranslator

---
+++ src/main/java/auctionsniper/AuctionMessageTranslator.java
@@ -0,0 +1,12 @@
+package auctionsniper;
+
+import org.jivesoftware.smack.Chat;
+import org.jivesoftware.smack.MessageListener;
+import org.jivesoftware.smack.packet.Message;
+
+public class AuctionMessageTranslator implements MessageListener {
+    @Override
+    public void processMessage(Chat chat, Message message) {
+
+    }
+}
