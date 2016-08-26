470a8f3a57e6a70220cea2d62aa6d1f7dab773e6
Chad Berchek <chadberchek@carfax.com>
2016-08-26 13:34:51 -0500

    Fix typo in price message from fake auction server

--- src/test/java/test/auctionsniper/FakeAuctionServer.java
+++ src/test/java/test/auctionsniper/FakeAuctionServer.java
@@ -54,7 +54,7 @@ public class FakeAuctionServer {
 
     public void reportPrice(int price, int increment, String bidder) throws XMPPException {
         currentChat.sendMessage(format("SOLVersion: 1.1; Event: PRICE; " +
-                        "CurrentPrice: %d; Increment %d; Bidder: %s;",
+                        "CurrentPrice: %d; Increment: %d; Bidder: %s;",
                 price, increment, bidder));
     }
 
