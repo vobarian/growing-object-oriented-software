0ad35381d0c61a3aa067c6adee0a1572dd5e6fe7
Chad Berchek <chadberchek@carfax.com>
2016-08-26 13:08:34 -0500

    p. 129 add Auction class for bidding and SniperListener.sniperBidding to notify when sniper is bidding; implement Main.sniperBidding to display bidding status label

--- src/test/java/test/auctionsniper/AuctionSniperTest.java
+++ src/test/java/test/auctionsniper/AuctionSniperTest.java
@@ -1,5 +1,6 @@
 package test.auctionsniper;
 
+import auctionsniper.Auction;
 import auctionsniper.AuctionSniper;
 import auctionsniper.SniperListener;
 import org.jmock.Expectations;
@@ -12,13 +13,25 @@ import org.junit.runner.RunWith;
 public class AuctionSniperTest {
     private final Mockery context = new Mockery();
     private final SniperListener sniperListener = context.mock(SniperListener.class);
-    private final AuctionSniper sniper = new AuctionSniper(sniperListener);
+    private final Auction auction = context.mock(Auction.class);
+    private final AuctionSniper sniper = new AuctionSniper(auction, sniperListener);
 
     @Test public void reportsLostWhenAuctionCloses() {
         context.checking(new Expectations() {{
-            one(sniperListener).sniperLost();
+            atLeast(1).of(sniperListener).sniperLost();
         }});
 
         sniper.auctionClosed();
+    }
+
+    @Test public void bidsHigherAndReportsBiddingWhenNewPriceArrives() {
+        final int price = 1001;
+        final int increment = 25;
+        context.checking(new Expectations() {{
+            one(auction).bid(price + increment);
+            atLeast(1).of(sniperListener).sniperBidding();
+        }});
+
+        sniper.currentPrice(price, increment);
     }
 }
