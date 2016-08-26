e5fb1057d1186a91a7ee629550cef2fe16bff924
Chad Berchek <chadberchek@carfax.com>
2016-08-26 13:08:34 -0500

    p. 129 add Auction class for bidding and SniperListener.sniperBidding to notify when sniper is bidding; implement Main.sniperBidding to display bidding status label
    Fix typo in price message from fake auction server

--- src/main/java/auctionsniper/AuctionSniper.java
+++ src/main/java/auctionsniper/AuctionSniper.java
@@ -2,8 +2,10 @@ package auctionsniper;
 
 public class AuctionSniper implements AuctionEventListener {
     private final SniperListener sniperListener;
+    private final Auction auction;
 
-    public AuctionSniper(SniperListener sniperListener) {
+    public AuctionSniper(Auction auction, SniperListener sniperListener) {
+        this.auction = auction;
         this.sniperListener = sniperListener;
     }
 
@@ -14,6 +16,7 @@ public class AuctionSniper implements AuctionEventListener {
 
     @Override
     public void currentPrice(int price, int increment) {
-
+        auction.bid(price + increment);
+        sniperListener.sniperBidding();
     }
 }
