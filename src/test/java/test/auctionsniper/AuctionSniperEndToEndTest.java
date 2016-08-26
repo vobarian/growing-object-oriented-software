3b9b095a396e6ebd3fc49d676c214159840176c4
Chad Berchek <chadberchek@carfax.com>
2016-08-26 14:31:49 -0500

    p. 140 end-to-end test for winning

--- src/test/java/test/auctionsniper/AuctionSniperEndToEndTest.java
+++ src/test/java/test/auctionsniper/AuctionSniperEndToEndTest.java
@@ -30,6 +30,24 @@ public class AuctionSniperEndToEndTest {
         application.showsSniperHasLostAuction();
     }
 
+    @Test public void sniperWinsAnAuctionByBiddingHigher() throws Exception {
+        auction.startSellingItem();
+
+        application.startBiddingIn(auction);
+        auction.hasReceivedJoinRequestFrom(ApplicationRunner.SNIPER_XMPP_ID);
+
+        auction.reportPrice(1000, 98, "other bidder");
+        application.hasShownSniperIsBidding();
+
+        auction.hasReceivedBid(1098, ApplicationRunner.SNIPER_XMPP_ID);
+
+        auction.reportPrice(1098, 97, ApplicationRunner.SNIPER_XMPP_ID);
+        application.hasShownSniperIsWinning();
+
+        auction.announceClosed();
+        application.showsSniperHasWonAuction();
+    }
+
     @After public void stopAuction() {
         auction.stop();
     }
