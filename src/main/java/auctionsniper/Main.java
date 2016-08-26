0bf8cc0a71aafc0e27894da61cfacec8479eac40
Chad Berchek <chadberchek@carfax.com>
2016-08-26 11:44:06 -0500

    p. 125 retrofit Main to use AuctionSniper and implement SniperListener instead of AuctionEventListener

--- src/main/java/auctionsniper/Main.java
+++ src/main/java/auctionsniper/Main.java
@@ -1,16 +1,14 @@
 package auctionsniper;
 
 import org.jivesoftware.smack.Chat;
-import org.jivesoftware.smack.MessageListener;
 import org.jivesoftware.smack.XMPPConnection;
 import org.jivesoftware.smack.XMPPException;
-import org.jivesoftware.smack.packet.Message;
 
 import javax.swing.SwingUtilities;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
-public class Main implements AuctionEventListener {
+public class Main implements SniperListener {
     private static final int ARG_HOSTNAME = 0;
     private static final int ARG_USERNAME = 1;
     private static final int ARG_PASSWORD = 2;
@@ -38,24 +36,20 @@ public class Main implements AuctionEventListener {
         disconnectWhenUICloses(connection);
         final Chat chat = connection.getChatManager().createChat(
                 auctionId(itemId, connection),
-                new AuctionMessageTranslator(this));
+                new AuctionMessageTranslator(new AuctionSniper(this)));
         this.notToBeGCd = chat;
 
         chat.sendMessage(JOIN_COMMAND_FORMAT);
     }
 
-    public void auctionClosed() {
+    @Override
+    public void sniperLost() {
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
                 ui.showStatus(MainWindow.STATUS_LOST);
             }
         });
-    }
-
-    @Override
-    public void currentPrice(int price, int increment) {
-
     }
 
     private void disconnectWhenUICloses(final XMPPConnection connection) {
