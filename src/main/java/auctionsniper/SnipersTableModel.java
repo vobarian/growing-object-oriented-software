6ab576cff15fabc1a57c35d3198ca46b9a0a1f8e
Chad Berchek <chadberchek@carfax.com>
2016-09-02 16:26:46 -0500

    p. 158 implement SnipersTableModel

--- src/main/java/auctionsniper/SnipersTableModel.java
+++ src/main/java/auctionsniper/SnipersTableModel.java
@@ -5,11 +5,13 @@ import javax.swing.table.AbstractTableModel;
 import static auctionsniper.MainWindow.STATUS_JOINING;
 
 public class SnipersTableModel extends AbstractTableModel {
+    private final static SniperState STARTING_UP = new SniperState("", 0, 0);
     private String statusText = STATUS_JOINING;
+    private SniperState sniperState = STARTING_UP;
 
     @Override
     public int getColumnCount() {
-        return 1;
+        return Column.values().length;
     }
 
     @Override
@@ -19,7 +21,18 @@ public class SnipersTableModel extends AbstractTableModel {
 
     @Override
     public Object getValueAt(int rowIndex, int columnIndex) {
-        return statusText;
+        switch (Column.at(columnIndex)) {
+            case ITEM_IDENTIFIER:
+                return sniperState.itemId;
+            case LAST_PRICE:
+                return sniperState.lastPrice;
+            case LAST_BID:
+                return sniperState.lastBid;
+            case SNIPER_STATUS:
+                return statusText;
+            default:
+                throw new IllegalArgumentException("No column at " + columnIndex);
+        }
     }
 
     public void setStatusText(String newStatusText) {
@@ -28,5 +41,8 @@ public class SnipersTableModel extends AbstractTableModel {
     }
 
     public void sniperStatusChanged(SniperState newSniperState, String newStatusText) {
+        sniperState = newSniperState;
+        statusText = newStatusText;
+        fireTableRowsUpdated(0, 0);
     }
 }
