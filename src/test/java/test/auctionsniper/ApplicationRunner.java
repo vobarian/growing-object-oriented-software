df4203d104b4ffd0394731a07bcda4dc2bbf1b4b
Chad Berchek <chadberchek@carfax.com>
2016-09-02 15:20:33 -0500

    p. 153 update win scenario end to end test to require item ID, last price, and last bid to be displayed in the table

--- src/test/java/test/auctionsniper/ApplicationRunner.java
+++ src/test/java/test/auctionsniper/ApplicationRunner.java
@@ -9,8 +9,10 @@ public class ApplicationRunner {
     public static final String SNIPER_PASSWORD = "sniper";
     public static final String SNIPER_XMPP_ID = SNIPER_ID + "@" + XMPP_HOSTNAME + "/Auction";
     private AuctionSniperDriver driver;
+    private String itemId;
 
     public void startBiddingIn(final FakeAuctionServer auction) {
+        itemId = auction.getItemId();
         Thread thread = new Thread("Test Application") {
             @Override
             public void run() {
@@ -31,16 +33,20 @@ public class ApplicationRunner {
         driver.showsSniperStatus(MainWindow.STATUS_BIDDING);
     }
 
-    public void hasShownSniperIsWinning() {
-        driver.showsSniperStatus(MainWindow.STATUS_WINNING);
+    public void hasShownSniperIsBidding(int lastPrice, int lastBid) {
+        driver.showsSniperStatus(itemId, lastPrice, lastBid, MainWindow.STATUS_BIDDING);
+    }
+
+    public void hasShownSniperIsWinning(int winningBid) {
+        driver.showsSniperStatus(itemId, winningBid, winningBid, MainWindow.STATUS_WINNING);
     }
 
     public void showsSniperHasLostAuction() {
         driver.showsSniperStatus(MainWindow.STATUS_LOST);
     }
 
-    public void showsSniperHasWonAuction() {
-        driver.showsSniperStatus(MainWindow.STATUS_WON);
+    public void showsSniperHasWonAuction(int lastPrice) {
+        driver.showsSniperStatus(itemId, lastPrice, lastPrice, MainWindow.STATUS_WON);
     }
 
     public void stop() {
--- src/test/java/test/auctionsniper/AuctionSniperDriver.java
+++ src/test/java/test/auctionsniper/AuctionSniperDriver.java
@@ -6,7 +6,9 @@ import com.objogate.wl.swing.driver.JFrameDriver;
 import com.objogate.wl.swing.driver.JTableDriver;
 import com.objogate.wl.swing.gesture.GesturePerformer;
 
+import static com.objogate.wl.swing.matcher.IterableComponentsMatcher.matching;
 import static com.objogate.wl.swing.matcher.JLabelTextMatcher.withLabelText;
+import static java.lang.String.valueOf;
 import static org.hamcrest.CoreMatchers.equalTo;
 
 public class AuctionSniperDriver extends JFrameDriver {
@@ -20,5 +22,11 @@ public class AuctionSniperDriver extends JFrameDriver {
 
     public void showsSniperStatus(String statusText) {
         new JTableDriver(this).hasCell(withLabelText(equalTo(statusText)));
+    }
+
+    public void showsSniperStatus(String itemId, int lastPrice, int lastBid, String statusText) {
+        JTableDriver table = new JTableDriver(this);
+        table.hasRow(matching(withLabelText(itemId), withLabelText(valueOf(lastPrice)),
+                withLabelText(valueOf(lastBid)), withLabelText(statusText)));
     }
 }
