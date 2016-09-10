477b09c2a615c0db31aaf6415d0aed2dd48fab90
Chad Berchek <chadberchek@carfax.com>
2016-09-09 22:07:49 -0500

    p. 165 express won/lost logic as polymorphic method of SniperState

--- src/main/java/auctionsniper/SniperState.java
+++ src/main/java/auctionsniper/SniperState.java
@@ -1,9 +1,19 @@
 package auctionsniper;
 
 public enum SniperState {
-    JOINING,
-    BIDDING,
-    WINNING,
+    JOINING {
+        @Override public SniperState whenAuctionClosed() { return LOST; }
+    },
+    BIDDING {
+        @Override public SniperState whenAuctionClosed() { return LOST; }
+    },
+    WINNING {
+        @Override public SniperState whenAuctionClosed() { return WON; }
+    },
     LOST,
     WON;
+
+    public SniperState whenAuctionClosed() {
+        throw new Defect("Auction is already closed");
+    }
 }
