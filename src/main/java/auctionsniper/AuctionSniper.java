941d4739204abef6e4121a4e37237b526f33265b
Chad Berchek <chadberchek@carfax.com>
2016-09-09 20:13:50 -0500

    p. 163 sniper sends snapshot when winning instead of using sniperWinning() method; maintain last bid and price state

--- src/main/java/auctionsniper/AuctionSniper.java
+++ src/main/java/auctionsniper/AuctionSniper.java
@@ -1,15 +1,15 @@
 package auctionsniper;
 
 public class AuctionSniper implements AuctionEventListener {
-    private String itemId;
     private final SniperListener sniperListener;
     private final Auction auction;
     private boolean isWinning = false;
+    private SniperSnapshot snapshot;
 
     public AuctionSniper(String itemId, Auction auction, SniperListener sniperListener) {
-        this.itemId = itemId;
         this.auction = auction;
         this.sniperListener = sniperListener;
+        this.snapshot = SniperSnapshot.joining(itemId);
     }
 
     @Override
@@ -25,12 +25,12 @@ public class AuctionSniper implements AuctionEventListener {
     public void currentPrice(int price, int increment, PriceSource priceSource) {
         isWinning = priceSource == PriceSource.FromSniper;
         if (isWinning) {
-            sniperListener.sniperWinning();
+            snapshot = snapshot.winning(price);
         } else {
             int bid = price + increment;
             auction.bid(bid);
-            sniperListener.sniperStateChanged(
-                    new SniperSnapshot(itemId, price, bid, SniperState.BIDDING));
+            snapshot = snapshot.bidding(price, bid);
         }
+        sniperListener.sniperStateChanged(snapshot);
     }
 }
