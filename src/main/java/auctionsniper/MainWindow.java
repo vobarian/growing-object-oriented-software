67a6f4dac01dacb7a7efb50a9364b8103c7aae18
Chad Berchek <chadberchek@carfax.com>
2016-09-09 22:55:59 -0500

    p. 166 move status strings into table model; get rid of separate state string field

--- src/main/java/auctionsniper/MainWindow.java
+++ src/main/java/auctionsniper/MainWindow.java
@@ -9,12 +9,7 @@ import java.awt.Container;
 public class MainWindow extends JFrame {
     public static final String APPLICATION_TITLE = "Auction Sniper";
     public static final String MAIN_WINDOW_NAME = "Auction Sniper Main";
-    public static final String STATUS_JOINING = "Joining";
-    public static final String STATUS_LOST = "Lost";
-    public static final String STATUS_WON = "Won";
     private static final String SNIPERS_TABLE_NAME = "Snipers Table";
-    public static final String STATUS_BIDDING = "Bidding";
-    public static final String STATUS_WINNING = "Winning";
 
     private final SnipersTableModel snipers = new SnipersTableModel();
 
@@ -38,10 +33,6 @@ public class MainWindow extends JFrame {
         final JTable snipersTable = new JTable(snipers);
         snipersTable.setName(SNIPERS_TABLE_NAME);
         return snipersTable;
-    }
-
-    public void showStatusText(String statusText) {
-        snipers.setStatusText(statusText);
     }
 
     public void sniperStateChanged(SniperSnapshot snapshot) {
