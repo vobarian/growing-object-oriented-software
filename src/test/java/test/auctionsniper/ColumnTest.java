4f357134bf4ee0c3e720655822e47901832e3e38
Chad Berchek <chad@vobarian.com>
2016-09-17 17:24:21 -0500

    p. 167 add Column test; copied from https://github.com/sf105/goos-code/blob/master/test/unit/test/auctionsniper/ui/ColumnTest.java

---
+++ src/test/java/test/auctionsniper/ColumnTest.java
@@ -0,0 +1,21 @@
+package test.auctionsniper;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import auctionsniper.SniperSnapshot;
+import auctionsniper.SniperState;
+import auctionsniper.Column;
+
+public class ColumnTest {
+
+    @Test public void
+    retrievesValuesFromASniperSnapshot() {
+        SniperSnapshot snapshot = new SniperSnapshot("item", 123, 34, SniperState.BIDDING);
+        assertEquals("item", Column.ITEM_IDENTIFIER.valueIn(snapshot));
+        assertEquals(123, Column.LAST_PRICE.valueIn(snapshot));
+        assertEquals(34, Column.LAST_BID.valueIn(snapshot));
+        assertEquals("Bidding", Column.SNIPER_STATE.valueIn(snapshot));
+    }
+}
