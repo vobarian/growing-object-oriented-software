c2318eaec12115781041e3734ef8df0696988558
Chad Berchek <chad@vobarian.com>
2016-09-17 16:20:40 -0500

    p. 167 object-oriented column

--- src/main/java/auctionsniper/Column.java
+++ src/main/java/auctionsniper/Column.java
@@ -1,10 +1,28 @@
 package auctionsniper;
 
 public enum Column {
-    ITEM_IDENTIFIER,
-    LAST_PRICE,
-    LAST_BID,
-    SNIPER_STATE;
+    ITEM_IDENTIFIER {
+        @Override public Object valueIn(SniperSnapshot snapshot) {
+            return snapshot.itemId;
+        }
+    },
+    LAST_PRICE {
+        @Override public Object valueIn(SniperSnapshot snapshot) {
+            return snapshot.lastPrice;
+        }
+    },
+    LAST_BID {
+        @Override public Object valueIn(SniperSnapshot snapshot) {
+            return snapshot.lastBid;
+        }
+    },
+    SNIPER_STATE {
+        @Override public Object valueIn(SniperSnapshot snapshot) {
+            return SnipersTableModel.textFor(snapshot.state);
+        }
+    };
+
+    abstract public Object valueIn(SniperSnapshot snapshot);
 
     public static Column at(int offset) {
         return values()[offset];
