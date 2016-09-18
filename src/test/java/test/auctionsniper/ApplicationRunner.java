dcd8afc17b6ea077a25a3b20fe144bec024334d2
Chad Berchek <chad@vobarian.com>
2016-09-17 22:21:02 -0500

    p. 177 add test with second auction; make startBiddingIn accept multiple auctions and pass their IDs to Main
    Note that on line 35 of ApplicationRunner, in the call to driver.showsSniperStatus, the first argument is empty string ("") instead of auction.getItemId() as shown in the book. There is no code to show the ID upon initially joining yet, so the code as in the book fails all tests and produces a different error message than expected.

--- src/test/java/test/auctionsniper/ApplicationRunner.java
+++ src/test/java/test/auctionsniper/ApplicationRunner.java
@@ -13,12 +13,12 @@ public class ApplicationRunner {
     public static final String SNIPER_XMPP_ID = SNIPER_ID + "@" + XMPP_HOSTNAME + "/Auction";
     private AuctionSniperDriver driver;
 
-    public void startBiddingIn(final FakeAuctionServer auction) {
+    public void startBiddingIn(final FakeAuctionServer... auctions) {
         Thread thread = new Thread("Test Application") {
             @Override
             public void run() {
                 try {
-                    Main.main(XMPP_HOSTNAME, SNIPER_ID, SNIPER_PASSWORD, auction.getItemId());
+                    Main.main(arguments(auctions));
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
@@ -30,7 +30,21 @@ public class ApplicationRunner {
         driver = new AuctionSniperDriver(1000);
         driver.hasTitle(MainWindow.APPLICATION_TITLE);
         driver.hasColumnTitles();
-        driver.showsSniperStatus("", 0, 0, textFor(SniperState.JOINING));
+
+        for (FakeAuctionServer auction : auctions) {
+            driver.showsSniperStatus("", 0, 0, textFor(SniperState.JOINING));
+        }
+    }
+
+    protected static String[] arguments(FakeAuctionServer... auctions) {
+        String[] arguments = new String[auctions.length + 3];
+        arguments[0] = XMPP_HOSTNAME;
+        arguments[1] = SNIPER_ID;
+        arguments[2] = SNIPER_PASSWORD;
+        for (int i = 0; i < auctions.length; i++) {
+            arguments[i + 3] = auctions[i].getItemId();
+        }
+        return arguments;
     }
 
     public void hasShownSniperIsBidding(FakeAuctionServer auction, int lastPrice, int lastBid) {
