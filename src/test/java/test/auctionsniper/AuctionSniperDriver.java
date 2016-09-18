adceb1f4300747b5ac2dec7449325fc6451e5b30
Chad Berchek <chad@vobarian.com>
2016-09-17 21:17:14 -0500

    p. 169 add test for column headers

--- src/test/java/test/auctionsniper/AuctionSniperDriver.java
+++ src/test/java/test/auctionsniper/AuctionSniperDriver.java
@@ -4,12 +4,14 @@ import auctionsniper.MainWindow;
 import com.objogate.wl.swing.AWTEventQueueProber;
 import com.objogate.wl.swing.driver.JFrameDriver;
 import com.objogate.wl.swing.driver.JTableDriver;
+import com.objogate.wl.swing.driver.JTableHeaderDriver;
 import com.objogate.wl.swing.gesture.GesturePerformer;
 
 import static com.objogate.wl.swing.matcher.IterableComponentsMatcher.matching;
 import static com.objogate.wl.swing.matcher.JLabelTextMatcher.withLabelText;
 import static java.lang.String.valueOf;
 import static org.hamcrest.CoreMatchers.equalTo;
+import javax.swing.table.JTableHeader;
 
 public class AuctionSniperDriver extends JFrameDriver {
     public AuctionSniperDriver(int timeoutMillis) {
@@ -24,5 +26,11 @@ public class AuctionSniperDriver extends JFrameDriver {
         JTableDriver table = new JTableDriver(this);
         table.hasRow(matching(withLabelText(itemId), withLabelText(valueOf(lastPrice)),
                 withLabelText(valueOf(lastBid)), withLabelText(statusText)));
+    }
+
+    public void hasColumnTitles() {
+        JTableHeaderDriver headers = new JTableHeaderDriver(this, JTableHeader.class);
+        headers.hasHeaders(matching(withLabelText("Item"), withLabelText("Last Price"),
+                withLabelText("Last Bid"), withLabelText("State")));
     }
 }
