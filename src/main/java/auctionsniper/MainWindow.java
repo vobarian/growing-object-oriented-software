1bc608bd568d2a8a41e04e6f36641fba75f27d33
Chad Berchek <chad@vobarian.com>
2016-09-23 16:29:52 -0500

    p. 185 add item ID text field and new item button to UI

--- src/main/java/auctionsniper/MainWindow.java
+++ src/main/java/auctionsniper/MainWindow.java
@@ -1,10 +1,15 @@
 package auctionsniper;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.table.TableModel;
 import java.awt.BorderLayout;
 import java.awt.Container;
+import java.awt.FlowLayout;
 
 public class MainWindow extends JFrame {
     public static final String APPLICATION_TITLE = "Auction Sniper";
@@ -13,28 +18,39 @@ public class MainWindow extends JFrame {
     public static final String NEW_ITEM_ID_NAME = "item id";
     public static final String JOIN_BUTTON_NAME = "join button";
 
-    private final SnipersTableModel snipers;
-
-    public MainWindow(SnipersTableModel snipersTableModel) {
+    public MainWindow(TableModel snipers) {
         super(APPLICATION_TITLE);
-        this.snipers = snipersTableModel;
         setName(MAIN_WINDOW_NAME);
-        fillContentPane(makeSnipersTable());
+        fillContentPane(makeSnipersTable(snipers), makeControls());
         pack();
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
     }
 
-    private void fillContentPane(JTable snipersTable) {
+    private void fillContentPane(JTable snipersTable, JPanel controls) {
         final Container contentPane = getContentPane();
         contentPane.setLayout(new BorderLayout());
-
+        contentPane.add(controls, BorderLayout.NORTH);
         contentPane.add(new JScrollPane(snipersTable), BorderLayout.CENTER);
     }
 
-    private JTable makeSnipersTable() {
+    private JTable makeSnipersTable(TableModel snipers) {
         final JTable snipersTable = new JTable(snipers);
         snipersTable.setName(SNIPERS_TABLE_NAME);
         return snipersTable;
+    }
+
+    private JPanel makeControls() {
+        JPanel controls = new JPanel(new FlowLayout());
+        final JTextField itemIdField = new JTextField();
+        itemIdField.setColumns(25);
+        itemIdField.setName(NEW_ITEM_ID_NAME);
+        controls.add(itemIdField);
+
+        JButton joinAuctionButton = new JButton("Join Auction");
+        joinAuctionButton.setName(JOIN_BUTTON_NAME);
+        controls.add(joinAuctionButton);
+
+        return controls;
     }
 }
