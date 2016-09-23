faa6fd605bf620a75f2b7c4fb5eaf21e2bc0f680
Chad Berchek <chad@vobarian.com>
2016-09-23 17:03:41 -0500

    p. 186 integration test MainWindow calls UserRequestListener when new item button clicked

---
+++ src/test/java/test/auctionsniper/MainWindowTest.java
@@ -0,0 +1,29 @@
+package test.auctionsniper;
+
+import com.objogate.wl.swing.probe.ValueMatcherProbe;
+import auctionsniper.MainWindow;
+import auctionsniper.SnipersTableModel;
+import auctionsniper.UserRequestListener;
+import static org.hamcrest.CoreMatchers.equalTo;
+import org.junit.Test;
+
+public class MainWindowTest {
+    private final SnipersTableModel tableModel = new SnipersTableModel();
+    private final MainWindow mainWindow = new MainWindow(tableModel);
+    private final AuctionSniperDriver driver = new AuctionSniperDriver(100);
+
+    @Test public void makesUserRequestWhenJoinButtonClicked() {
+        final ValueMatcherProbe<String> buttonProbe =
+                new ValueMatcherProbe<>(equalTo("an item-id"), "join request");
+
+        mainWindow.addUserRequestListener(
+                new UserRequestListener() {
+                    public void joinAuction(String itemId) {
+                        buttonProbe.setReceivedValue(itemId);
+                    }
+                });
+
+        driver.startBiddingFor("an item-id");
+        driver.check(buttonProbe);
+    }
+}
