6d841c0f03c627872424b84cbba145ab34dc1976
Chad Berchek <chad@vobarian.com>
2016-09-23 16:22:02 -0500

    p. 184 test add items through UI

--- src/test/java/test/auctionsniper/AuctionSniperDriver.java
+++ src/test/java/test/auctionsniper/AuctionSniperDriver.java
@@ -2,15 +2,19 @@ package test.auctionsniper;
 
 import auctionsniper.MainWindow;
 import com.objogate.wl.swing.AWTEventQueueProber;
+import com.objogate.wl.swing.driver.JButtonDriver;
 import com.objogate.wl.swing.driver.JFrameDriver;
 import com.objogate.wl.swing.driver.JTableDriver;
 import com.objogate.wl.swing.driver.JTableHeaderDriver;
+import com.objogate.wl.swing.driver.JTextFieldDriver;
 import com.objogate.wl.swing.gesture.GesturePerformer;
 
 import static com.objogate.wl.swing.matcher.IterableComponentsMatcher.matching;
 import static com.objogate.wl.swing.matcher.JLabelTextMatcher.withLabelText;
 import static java.lang.String.valueOf;
 import static org.hamcrest.CoreMatchers.equalTo;
+import javax.swing.JButton;
+import javax.swing.JTextField;
 import javax.swing.table.JTableHeader;
 
 public class AuctionSniperDriver extends JFrameDriver {
@@ -20,6 +24,21 @@ public class AuctionSniperDriver extends JFrameDriver {
                         named(MainWindow.MAIN_WINDOW_NAME),
                         showingOnScreen()),
                 new AWTEventQueueProber(timeoutMillis, 100));
+    }
+
+    public void startBiddingFor(String itemId) {
+        itemIdField().replaceAllText(itemId);
+        bidButton().click();
+    }
+
+    private JTextFieldDriver itemIdField() {
+        JTextFieldDriver newItemId = new JTextFieldDriver(this, JTextField.class, named(MainWindow.NEW_ITEM_ID_NAME));
+        newItemId.focusWithMouse();
+        return newItemId;
+    }
+
+    private JButtonDriver bidButton() {
+        return new JButtonDriver(this, JButton.class, named(MainWindow.JOIN_BUTTON_NAME));
     }
 
     public void showsSniperStatus(String itemId, int lastPrice, int lastBid, String statusText) {
