5af2a916cbe3b55a211fe9c9a33d0081e72c5578
Chad Berchek <chad@vobarian.com>
2016-09-30 22:48:21 -0500

    p. 195 add XMPPAuction integration test that actually connects to chat server and checks that listener is called

---
+++ src/integration/java/test/integration/auctionsniper/xmpp/XMPPAuctionTest.java
@@ -0,0 +1,61 @@
+package test.integration.auctionsniper.xmpp;
+
+import java.util.concurrent.CountDownLatch;
+import org.jivesoftware.smack.XMPPConnection;
+import org.jivesoftware.smack.XMPPException;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import auctionsniper.Auction;
+import auctionsniper.AuctionEventListener;
+import auctionsniper.Main;
+import auctionsniper.xmpp.XMPPAuction;
+import test.endtoend.auctionsniper.ApplicationRunner;
+import test.endtoend.auctionsniper.FakeAuctionServer;
+import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.junit.Assert.assertTrue;
+
+public class XMPPAuctionTest {
+    private final FakeAuctionServer auctionServer = new FakeAuctionServer("item-54321");
+    private XMPPConnection connection;
+
+    @Before public void openConnection() throws XMPPException {
+        connection = new XMPPConnection(FakeAuctionServer.XMPP_HOSTNAME);
+        connection.connect();
+        connection.login(ApplicationRunner.SNIPER_ID, ApplicationRunner.SNIPER_PASSWORD, Main.AUCTION_RESOURCE);
+    }
+
+    @After public void closeConnection() {
+        connection.disconnect();
+    }
+
+    @Before public void startAuction() throws XMPPException {
+        auctionServer.startSellingItem();
+    }
+
+    @After public void stopAuction() {
+        auctionServer.stop();
+    }
+
+    @Test public void receivesEventsFromAuctionServerAfterJoining() throws Exception {
+        CountDownLatch auctionWasClosed = new CountDownLatch(1);
+
+        Auction auction = new XMPPAuction(connection, auctionServer.getItemId());
+        auction.addAuctionEventListener(auctionClosedListener(auctionWasClosed));
+
+        auction.join();
+        auctionServer.hasReceivedJoinRequestFrom(ApplicationRunner.SNIPER_XMPP_ID);
+        auctionServer.announceClosed();
+
+        assertTrue("should have been closed", auctionWasClosed.await(2, SECONDS));
+    }
+
+    private AuctionEventListener auctionClosedListener(final CountDownLatch auctionWasClosed) {
+        return new AuctionEventListener() {
+            @Override
+            public void auctionClosed() { auctionWasClosed.countDown(); }
+            @Override
+            public void currentPrice(int price, int increment, PriceSource priceSource) { }
+        };
+    }
+}
