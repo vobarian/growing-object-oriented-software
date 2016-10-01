5439e9fce387442d17804ea20c4a74ff3e14415e
Chad Berchek <chad@vobarian.com>
2016-09-30 22:14:35 -0500

    p. 194 encapsulate Chat within XMPPAuction

--- src/main/java/auctionsniper/XMPPAuction.java
+++ src/main/java/auctionsniper/XMPPAuction.java
@@ -1,14 +1,21 @@
 package auctionsniper;
 
 import org.jivesoftware.smack.Chat;
+import org.jivesoftware.smack.XMPPConnection;
 import org.jivesoftware.smack.XMPPException;
+import auctionsniper.util.Announcer;
 import static java.lang.String.format;
 
 public class XMPPAuction implements Auction {
+
+    private final Announcer<AuctionEventListener> auctionEventListeners =
+            Announcer.to(AuctionEventListener.class);
     private final Chat chat;
 
-    public XMPPAuction(Chat chat) {
-        this.chat = chat;
+    public XMPPAuction(XMPPConnection connection, String itemId) {
+        chat = connection.getChatManager().createChat(
+                auctionId(itemId, connection),
+                new AuctionMessageTranslator(connection.getUser(), auctionEventListeners.announce()));
     }
 
     public void bid(int amount) {
@@ -19,11 +26,20 @@ public class XMPPAuction implements Auction {
         sendMessage(Main.JOIN_COMMAND_FORMAT);
     }
 
+    @Override
+    public void addAuctionEventListener(AuctionEventListener listener) {
+        auctionEventListeners.addListener(listener);
+    }
+
     private void sendMessage(final String message) {
         try {
             chat.sendMessage(message);
         } catch (XMPPException e) {
             e.printStackTrace();
         }
+    }
+
+    private static String auctionId(String itemId, XMPPConnection connection) {
+        return String.format(Main.AUCTION_ID_FORMAT, itemId, connection.getServiceName());
     }
 }
