269c999198304df03170f78cf8f7d925eda4ec6f
Chad Berchek <chad@vobarian.com>
2016-10-07 00:21:33 -0500

    p. 198, 199 move responsibility for registering Swing sniper listener and notToBeGCd into SnipersTableModel instead of SniperLauncher

--- src/main/java/auctionsniper/ui/SwingThreadSniperListener.java
+++ src/main/java/auctionsniper/ui/SwingThreadSniperListener.java
@@ -1,6 +1,8 @@
-package auctionsniper;
+package auctionsniper.ui;
 
 import javax.swing.SwingUtilities;
+import auctionsniper.SniperListener;
+import auctionsniper.SniperSnapshot;
 
 public class SwingThreadSniperListener implements SniperListener {
     private final SniperListener delegate;
