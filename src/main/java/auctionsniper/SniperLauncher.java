eeab91e7cf9fb3ecd730583952c4cfa6c4116706
Chad Berchek <chad@vobarian.com>
2016-10-06 23:11:59 -0500

    p. 197 extract SniperLauncher

---
+++ src/main/java/auctionsniper/SniperLauncher.java
@@ -0,0 +1,25 @@
+package auctionsniper;
+
+import java.util.ArrayList;
+import auctionsniper.ui.SnipersTableModel;
+
+public class SniperLauncher implements UserRequestListener {
+    private final ArrayList<Auction> notToBeGCd = new ArrayList<>();
+    private final AuctionHouse auctionHouse;
+    private final SnipersTableModel snipers;
+
+    public SniperLauncher(AuctionHouse auctionHouse, SnipersTableModel snipers) {
+        this.auctionHouse = auctionHouse;
+        this.snipers = snipers;
+    }
+
+    @Override
+    public void joinAuction(String itemId) {
+        snipers.addSniper(SniperSnapshot.joining(itemId));
+        Auction auction = auctionHouse.auctionFor(itemId);
+        notToBeGCd.add(auction);
+        AuctionSniper sniper = new AuctionSniper(itemId, auction, new SwingThreadSniperListener(snipers));
+        auction.addAuctionEventListener(sniper);
+        auction.join();
+    }
+}
