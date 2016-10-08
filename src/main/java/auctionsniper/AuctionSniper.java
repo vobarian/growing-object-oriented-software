17d38e47149f41eb645daf6c3823d1b62e250df0
Chad Berchek <chad@vobarian.com>
2016-10-07 23:34:38 -0500

    p. 211 add stop price check; passes end-to-end test for losing and AuctionSniper unit tests

--- src/main/java/auctionsniper/AuctionSniper.java
+++ src/main/java/auctionsniper/AuctionSniper.java
@@ -6,8 +6,10 @@ public class AuctionSniper implements AuctionEventListener {
     private final Announcer<SniperListener> listeners = Announcer.to(SniperListener.class);
     private final Auction auction;
     private SniperSnapshot snapshot;
+    private final Item item;
 
     public AuctionSniper(Item item, Auction auction) {
+        this.item = item;
         this.auction = auction;
         this.snapshot = SniperSnapshot.joining(item.identifier);
     }
@@ -30,8 +32,12 @@ public class AuctionSniper implements AuctionEventListener {
                 break;
             case FromOtherBidder:
                 int bid = price + increment;
-                auction.bid(bid);
-                snapshot = snapshot.bidding(price, bid);
+                if (item.allowsBid(bid)) {
+                    auction.bid(bid);
+                    snapshot = snapshot.bidding(price, bid);
+                } else {
+                    snapshot = snapshot.losing(price);
+                }
                 break;
         }
         notifyChange();
