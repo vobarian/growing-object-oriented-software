eb303109274df79aa7638864b58b75ac61641f13
Chad Berchek <chad@vobarian.com>
2016-10-07 22:41:25 -0500

    p. 210 introduce Item class; update MainWindowTest to work with Items and pass it

--- src/test/java/test/integration/auctionsniper/ui/MainWindowTest.java
+++ src/test/java/test/integration/auctionsniper/ui/MainWindowTest.java
@@ -1,6 +1,7 @@
 package test.integration.auctionsniper.ui;
 
 import com.objogate.wl.swing.probe.ValueMatcherProbe;
+import auctionsniper.Item;
 import auctionsniper.SniperPortfolio;
 import auctionsniper.UserRequestListener;
 import auctionsniper.ui.MainWindow;
@@ -14,18 +15,18 @@ public class MainWindowTest {
     private final AuctionSniperDriver driver = new AuctionSniperDriver(100);
 
     @Test public void makesUserRequestWhenJoinButtonClicked() {
-        final ValueMatcherProbe<String> buttonProbe =
-                new ValueMatcherProbe<>(equalTo("an item-id"), "join request");
+        final ValueMatcherProbe<Item> itemProbe =
+                new ValueMatcherProbe<>(equalTo(new Item("an item-id", 789)), "item request");
 
         mainWindow.addUserRequestListener(
                 new UserRequestListener() {
-                    public void joinAuction(String itemId) {
-                        buttonProbe.setReceivedValue(itemId);
+                    public void joinAuction(Item item) {
+                        itemProbe.setReceivedValue(item);
                     }
                 });
 
-        driver.startBiddingFor("an item-id", Integer.MAX_VALUE);
-        driver.check(buttonProbe);
+        driver.startBiddingFor("an item-id", 789);
+        driver.check(itemProbe);
     }
 
     @After public void disposeDriver() {
