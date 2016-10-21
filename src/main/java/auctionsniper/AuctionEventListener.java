a3e9ef7ae73165ea535c3bf1fce5f5ef6ad0e230
chadberchek <chad@vobarian.com>
2016-10-21 13:04:41 -0500

    p. 217 failing unit test for auction message translator invoking auctionFailed on listener upon receiving bad message

--- src/main/java/auctionsniper/AuctionEventListener.java
+++ src/main/java/auctionsniper/AuctionEventListener.java
@@ -9,4 +9,5 @@ public interface AuctionEventListener extends EventListener {
 
     void auctionClosed();
     void currentPrice(int price, int increment, PriceSource priceSource);
+    void auctionFailed();
 }
