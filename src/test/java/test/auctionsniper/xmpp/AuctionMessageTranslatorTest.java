a148ff1be6345102b750e204ea5220ba0a673153
Chad Berchek <chad@vobarian.com>
2016-10-22 10:17:00 -0500

    p. 218 failing test for translation failure when missing fields in a message

--- src/test/java/test/auctionsniper/xmpp/AuctionMessageTranslatorTest.java
+++ src/test/java/test/auctionsniper/xmpp/AuctionMessageTranslatorTest.java
@@ -67,4 +67,15 @@ public class AuctionMessageTranslatorTest {
 
         translator.processMessage(UNUSED_CHAT, message);
     }
+
+    @Test public void notifiesAuctionFailedWhenEventTypeMissing() {
+        context.checking(new Expectations() {{
+            exactly(1).of(listener).auctionFailed();
+        }});
+
+        Message message = new Message();
+        message.setBody("SOLVersion: 1.1; CurrentPrice: 234; Increment: 5; Bidder: " + SNIPER_ID + ";");
+
+        translator.processMessage(UNUSED_CHAT, message);
+    }
 }
